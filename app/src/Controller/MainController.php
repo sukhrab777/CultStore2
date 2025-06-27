44ea2f4cf9ff22c553262633cb8b27d845b3abb0
yacine oumohand <yacine.oumohand@icloud.com>
2025-06-27 15:31:50 +0200

    recupération

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main')]
+    public function index(CategoriesRepository $categoriesRepository): Response
     {
+        $categories = $categoriesRepository->findAll();
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'categories' => $categories,
         ]);
     }
 }
