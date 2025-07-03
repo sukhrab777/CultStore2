926e6835e86f905a71cf24230c300e639d16b38c
da silva vieira mac <dasilvavieiracedric91@yahoo.com>
2025-07-03 10:23:15 +0200

    mise a jour

--- app/src/Controller/EventsController.php
+++ app/src/Controller/EventsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Services\EventService;
+use App\Service\EventService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
--- app/src/Service/EventService.php
+++ app/src/Service/EventService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
